36297c3f8c8e8f041dfa5ffafb6887dace36b0c1
3709565 <erik.nijland.osv@fedex.com>
2021-07-07 13:14:44 +0200

    Import the optimizelySDK. Errors start appearing when running ng build or ng serve.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as optimizelySDK from '@optimizely/optimizely-sdk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    console.log(optimizelySDK);
+  }
 }
